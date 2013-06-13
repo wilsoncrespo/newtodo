869226bab884c021813642b78fd16e917b405530
jorge.burgos <mtnburgos@yahoo.es>
2013-06-12 23:04:58 -0400

    Enable add todo capability.

--- WebContent/todo/js/app/models/todos.collection.js
+++ WebContent/todo/js/app/models/todos.collection.js
@@ -5,6 +5,13 @@ define([
     var TodosCollection = Backbone.Collection.extend({
         model: TodoModel,
         localStorage: new Backbone.LocalStorage("todos-backbone"),
+        
+        addNewModel: function(title) {
+            var ordered = this.size();
+            var todoModel = new TodoModel({title: title, ordered: ordered});
+            this.add(todoModel);
+        },
+        
     });
     
     return TodosCollection;
--- WebContent/todo/js/app/views/content.views.js
+++ WebContent/todo/js/app/views/content.views.js
@@ -6,7 +6,27 @@ define([
     var ContentViews = Backbone.Marionette.CompositeView.extend({
         template: _.template(ContentViewsTemplate),
         itemView: ContentView,
-        itemViewContainer: '#todo-list'
+        itemViewContainer: '#todo-list',
+        
+        initialize: function(){
+            _.bindAll(this,"createOnEnter");
+        },
+        
+        events: {
+            "keypress #new-todo": "createOnEnter",
+        },
+        
+        ui: {
+            title: '#new-todo',
+        },
+        
+        createOnEnter: function(e) {
+            if (e.keyCode != 13) return;
+            
+            var title = $.trim(this.ui.title.val());
+            this.collection.addNewModel(title);
+            this.ui.title.val("");
+        },
         
     });
     
